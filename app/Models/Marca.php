c6ff8ef9df63cccbbae6a17121a56733df92e1fc
WilliamJonathan <jonathanwilliam.ws@gmail.com>
2021-11-11 16:15:36 -0300

    migrations configuradas, resolvido problema com compatibiliade do MySQL 5.5

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+ * Para criar um model, controller, migration e popular controller com metodos padrões de uma só fez usar comando:
+ * php artisan make:model --migrate --controller --resource NomeModel
+ * ou abreviação
+ * php artisan make:model -mcr Nome
+ * ou comando all, que neste caso cria uma factory e um seeder tambem
+ * php artisan make:model (--all) ou (--a) Nome
+ */
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
